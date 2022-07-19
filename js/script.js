21e3d6e6fe15339386c8e1c660259105ce97cf97
Alex <kidajsjuda@gmail.com>
2022-07-19 12:26:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+"use strict";
+
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+
+// console.log("ширина кузова автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength); 
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+// console.log(arr[1]);
+
+
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+//  const result = confirm('Are you here?');
+//  console.log(result);
+
+
+// const answer = prompt("Are you 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('What is your age?', '');
+
+// document.write(answers);
+ 
+
+// const category = 'toys';
+
+// // console.log('https://someurl.com/' + category);
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`); - Интерполяция 
+
+
+let incr = 10,
+    decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
+
+// Если знаки поставить не в начале, а после, то покажет вначале старое значение. Поэтому лучше ставить перед.console
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || isClose);
